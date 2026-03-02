00794ae3933bf23223dc54ae8185e9c0422bf90b
wawan-maulana <wawanme07@gmail.com>
2026-03-02 09:32:28 +0700

    "update"

--- structur_data/pertemuan 3/pertemuan 3.py
+++ structur_data/pertemuan 3/pertemuan 3.py
@@ -1,4 +1,12 @@
 import sys
 senjata = ("kapak", "pedang", "panah")
 print(senjata)
-del senjata[1]
+print(senjata[1])
+senjata2 = []
+senjata2.append("tombak")
+print(senjata2)
+print(senjata2[0])
+print(sys.getsizeof(senjata))
+print(sys.getsizeof(senjata2))
+senjatabackup = senjata
+print(senjatabackup)
